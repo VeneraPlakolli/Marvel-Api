39fc3b9b714b5b2c3edaf6c472232c2107f4d881
VeneraPlakolli <94639395+VeneraPlakolli@users.noreply.github.com>
2022-12-19 12:14:40 +0100

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+
+Vue.use(VueRouter)
+
+
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    
+  </template>
+  <script>
+  import CardItem from './components/CardItem.vue' ;
+
+  export default {
+    name: 'home',
+    components: {
+      CardItem
+    },
+ 
+  }
+  </script>
